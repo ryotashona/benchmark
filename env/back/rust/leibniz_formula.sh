3d781f48785314f35d3cee314d4ae01e458f3cc4
ryosho2525 <ryosho.home@gmail.com>
2023-03-20 20:42:31 +0900

    フィボナッチ数列実装

--- env/back/rust/leibniz_formula.sh
+++ env/back/rust/leibniz_formula.sh
@@ -2,13 +2,6 @@
 
 out_file="/var/tmp/times.txt"
 
-if [ -f ${out_file} ]; then
-    rm ${out_file}
-fi
-
-cd /opt/leibniz_formula
-cargo build -r
-
 fmt="\nreal:%e[sec]\nuser:%U[sec]\nsys:%S[sec]\nMemory:%M[KB]"
 \time -o ${out_file} -f ${fmt} /opt/leibniz_formula/target/release/leibniz_formula
 
